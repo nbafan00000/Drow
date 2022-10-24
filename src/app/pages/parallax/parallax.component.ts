e4eb89db7052da635e0867c5d4181da428e79a36
PeleKomany <komanypele@outlook.com>
2022-10-24 16:11:42 +0800

    Adding FAQ

--- src/app/pages/parallax/parallax.component.ts
+++ src/app/pages/parallax/parallax.component.ts
@@ -7,6 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParallaxComponent implements OnInit {
 
+  testimonials = [
+    {
+      name: 'Anri',
+      date: '22 Sep, 2022',
+      title: 'Great product!',
+      description: 'I absolutely LOVE this product! my productivity increased, also I had better sleep last week! so everyone preorder it right now!',
+      rating: 5,
+    },
+    {
+    },
+    {
+    },
+  ];
+
+  faqs = [
+    {
+      question: "What's the best thing about Switzerland?",
+      answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    },
+    {
+      question: "This is second question of this area, and section?",
+      answer: "You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    },
+    {
+      question: "Why do you never see elephants hiding in trees?",
+      answer: "Because they're so good at it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat."
+    }
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
