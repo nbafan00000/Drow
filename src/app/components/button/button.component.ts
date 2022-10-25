c8f196f3a4d932a33403b8964380daac676e0467
PeleKomany <komanypele@outlook.com>
2022-10-25 23:48:46 +0800

    Fixing the button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -6,9 +6,23 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
-  @Input() bgColor: string = '#4C48F4';
+  @Input() type: string = 'primary';
   @Input() label: string = '';
+  hover: boolean = false;
+  @Input() disabled: boolean = false;
 
+  colors: { [key: string]: any } = {
+    'primary': {
+      'bgColor': '#4C48F4',
+      'hoverColor': '#4541E3',
+      'disabledColor': 'rgba(76, 72, 244, 0.2)',
+    },
+    'secondary': {
+      'bgColor': '#3F659D',
+      'hoverColor': '#36588A',
+      'disabledColor': 'rgba(63, 101, 157, 0.2)',
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
