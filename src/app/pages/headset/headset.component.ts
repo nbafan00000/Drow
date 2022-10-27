79a5e595bb8b23fd8a1c77be0ffdf5d77a4b99f2
PeleKomany <komanypele@outlook.com>
2022-10-27 11:42:17 +0800

    Styling for header

--- src/app/pages/headset/headset.component.ts
+++ src/app/pages/headset/headset.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./headset.component.scss']
 })
 export class HeadsetComponent implements OnInit {
-
+  descriptions = [
+    {
+      image: '../../../assets/head.svg',
+      title: 'Wear Our Device',
+      description: 'Sit comfortable and wear our device and then press start brain training.'
+    },
+    {
+      image: '../../../assets/eye-closed.svg',
+      title: 'Keep your eyes closed',
+      description: 'Listen session to calculate your sleep efficiency'
+    },
+    {
+      image: '../../../assets/efficiency.svg',
+      title: 'Calculating Efficiency',
+      description: 'Our A.I. calculates your expected sleep wellness for tonight. With further training, this number will increase over time'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
