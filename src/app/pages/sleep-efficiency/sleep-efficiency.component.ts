12635da27dcbd21672be1dc484c5456015af72de
PeleKomany <komanypele@outlook.com>
2022-11-01 16:32:38 +0800

    Quick fix

--- src/app/pages/sleep-efficiency/sleep-efficiency.component.ts
+++ src/app/pages/sleep-efficiency/sleep-efficiency.component.ts
@@ -15,17 +15,7 @@ export class SleepEfficiencyComponent implements OnInit {
   @HostListener('window:scroll', ['$event.target'])
   onScroll(event: any): void {
     const scrollTop = (event as Element).clientTop;
-
-    // const maxScrollTop = html.scrollHeight - window.innerHeight;
-    // const scrollFraction = scrollTop / maxScrollTop;
-    // const frameIndex = Math.min(
-    //   frameCount - 1,
-    //   Math.ceil(scrollFraction * frameCount)
-    // );
-
     const height = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) - (document.getElementById('home') as Element).clientHeight - 122;
-
     this.maskWidth = Math.min(Math.max(height /  ((document.getElementById('sleep-efficiency-scroll') as Element).clientHeight * 5 / 6.0) * 100, 0), 100);
-    console.log('---', this.maskWidth);
   }
 }
