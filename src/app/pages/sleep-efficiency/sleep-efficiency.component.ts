26a40786b154010bd4642d7c9465e987d9085f42
PeleKomany <komanypele@outlook.com>
2022-11-01 16:03:15 +0800

    Adding mask

--- src/app/pages/sleep-efficiency/sleep-efficiency.component.ts
+++ src/app/pages/sleep-efficiency/sleep-efficiency.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-sleep-efficiency',
@@ -8,8 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class SleepEfficiencyComponent implements OnInit {
 
   constructor() { }
-
+  maskWidth: number = 0;
   ngOnInit(): void {
   }
 
+  @HostListener('window:scroll', ['$event.target'])
+  onScroll(event: any): void {
+    const scrollTop = (event as Element).clientTop;
+
+    // const maxScrollTop = html.scrollHeight - window.innerHeight;
+    // const scrollFraction = scrollTop / maxScrollTop;
+    // const frameIndex = Math.min(
+    //   frameCount - 1,
+    //   Math.ceil(scrollFraction * frameCount)
+    // );
+
+    const height = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) - (document.getElementById('home') as Element).clientHeight - 122;
+
+    this.maskWidth = Math.min(Math.max(height /  ((document.getElementById('sleep-efficiency-scroll') as Element).clientHeight * 5 / 6.0) * 100, 0), 100);
+    console.log('---', this.maskWidth);
+  }
 }
