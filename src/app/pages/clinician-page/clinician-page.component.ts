fb930fc8c1a8c68eec511fbe443e6133131d84b0
PeleKomany <komanypele@outlook.com>
2022-11-07 15:38:12 +0800

    adding the modal

--- src/app/pages/clinician-page/clinician-page.component.ts
+++ src/app/pages/clinician-page/clinician-page.component.ts
@@ -6,6 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clinician-page.component.scss']
 })
 export class ClinicianPageComponent implements OnInit {
+  fields = [
+    {
+      id: 'full-name',
+      label: "Contact Person's Full Name",
+    },
+    {
+      id: 'clinic-name',
+      label: "Clinic Name",
+    },
+    {
+      id: 'contact-number',
+      label: "Clinic Contact Number",
+    },
+    {
+      id: 'clinic-address',
+      label: "Clinic Address",
+    },
+    {
+      id: 'email',
+      label: "Email",
+    }
+    
+  ];
 
   constructor() { }
   isModalOpen: boolean = false;
@@ -15,5 +38,9 @@ export class ClinicianPageComponent implements OnInit {
   
   openModal($event: number) {
     this.isModalOpen = true;
+  }
+
+  onClose() {
+    this.isModalOpen = false;
   }
 }
